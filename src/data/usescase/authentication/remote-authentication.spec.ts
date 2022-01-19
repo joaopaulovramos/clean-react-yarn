e35c020f0c0375914aa7946187e0a5a860172af0
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2022-01-19 17:55:06 -0300

    test: remote-authentication

---
+++ src/data/usescase/authentication/remote-authentication.spec.ts
@@ -0,0 +1,22 @@
+import { RemoteAuthentication } from './remote-authentication';
+import { HttpPostClient } from '../../protocols/http/http-post.client'
+
+describe('RemoteUthentication', () => {
+  test('Should call HttpPostClient with correct URL', async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string 
+      async post(url: string): Promise<void> {
+        this.url = url
+        return Promise.resolve()
+      }
+
+    }
+    const url = 'any_url'
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+
+})
+  
