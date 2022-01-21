0f7b7ad7405daf052dad9bba5f6aebc864162be0
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2022-01-21 13:13:49 -0300

    refactor: move logo to separate file

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -5,6 +5,7 @@ type SpinnerProps = React.HTMLAttributes<HTMLElement>
 
 const Spinner: React.FC<SpinnerProps> = (props: SpinnerProps) => {
   return (
+    // eslint-disable-next-line react/prop-types
     <div {...props} className={[StylesSheet.spinner, props.className].join(' ')}><div></div><div></div><div></div><div></div></div>
 
   )
