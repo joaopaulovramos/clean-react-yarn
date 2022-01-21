c116d99b06624329fa2083602c19dde0f19be7eb
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2022-01-21 20:18:08 -0300

    feat: add Routes

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,8 @@
+import Router from '@/presentation/components/router/router'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Login from '@/presentation/pages/login/login'
 
 ReactDOM.render(
-  <Login />,
+  <Router />,
   document.getElementById('main')
 )
