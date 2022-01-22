93edbf4a0e4e2a2cedf73f37720c09ce1cc00741
joaopaulovramos <59423604+joaopaulovramos@users.noreply.github.com>
2022-01-22 00:37:34 -0300

    feat: ensure Login Input start with inputs with required state

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React, { useContext } from 'react'
 import Styles from './input-styles.scss'
+import Context from '@/presentation/contexts/form-context'
 
 type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<InputProps> = (props: InputProps) => {
+  const { errorState } = useContext(Context)
+  // eslint-disable-next-line react/prop-types
+  const error = errorState[props.name]
+  const getStatus = (): string => {
+    return '🔴'
+  }
+  const getTitle = (): string => {
+    return error
+  }
   return (
     <div className={Styles.inputWrap}>
       <input {...props} />
-      <span className={Styles.status}>🔴</span>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -6,22 +6,20 @@ import Input from '@/presentation/components/input/input'
 import FormStatus from '@/presentation/components/form-status/form-status'
 import Context from '@/presentation/contexts/form-context'
 
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
-    isLoading: false,
-    errorMessage: ''
+  const [state] = useState({
+    isLoading: false
+  })
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
   })
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={state}>
+      <Context.Provider value={{ state, errorState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
